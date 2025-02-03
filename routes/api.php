620eb81cd82b6ae9e342de3252a3dd1a06cdea36
Shakaib Ishfaq <139215472+shakaib-fabtechsol@users.noreply.github.com>
2025-02-03 11:56:28 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,13 @@
 <?php
 
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\ServiceProviderController;
 
 Route::post('Register', [AuthController::class, 'Register'])->name('Register');
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ServiceProviderController;
 
 Route::controller(ServiceProviderController::class)->group(function () {
     Route::post('addDeal', 'addDeal')->name('AddDeal');
