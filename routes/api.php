71a468cf96b8624f6002e96dd0f4251075338593
AliHassanFabtechsol <alihassandaska@fabtechsol.com>
2025-02-05 18:18:27 +0500

    additional photo  api created

--- routes/api.php
+++ routes/api.php
@@ -13,41 +13,41 @@ Route::post('Userlogin','Userlogin')->name('Userlogin');
 
 });
 Route::middleware('auth:sanctum')->group( function () {
-    Route::controller(ServiceProviderController::class)->group(function () {
-        Route::post('BasicInfo', 'BasicInfo')->name('BasicInfo');
-        Route::post('UpdateBasicInfo', 'UpdateBasicInfo')->name('UpdateBasicInfo');
     
-        Route::post('PriceAndPackage', 'PriceAndPackage')->name('PriceAndPackage');
-        Route::post('UpdatePriceAndPackage', 'UpdatePriceAndPackage')->name('UpdatePriceAndPackage');
-    
-        Route::post('MediaUpload', 'MediaUpload')->name('MediaUpload');
-        Route::post('UpdateMediaUpload', 'UpdateMediaUpload')->name('UpdateMediaUpload');
-    
-        Route::get('Deals', 'Deals')->name('Deals');
-        Route::get('Deal/{id}', 'Deal')->name('Deal');
-        Route::get('DeleteDeal/{id}', 'DeleteDeal')->name('DeleteDeal');
-    
-        Route::post('DealPublish', 'DealPublish')->name('DealPublish');
-    
-    
-        Route::post('MyDetails', 'MyDetails')->name('MyDetails');
-    
-        Route::post('UpdatePassword', 'UpdatePassword')->name('UpdatePassword');
-    
-        Route::post('BusinessProfile', 'BusinessProfile')->name('BusinessProfile');
-    
-    
-        Route::post('AddPaymentDetails', 'AddPaymentDetails')->name('AddPaymentDetails');
-        Route::post('UpdatePaymentDetails', 'UpdatePaymentDetails')->name('UpdatePaymentDetails');
-
-        Route::get('DeletePaymentDetails/{id}', 'DeletePaymentDetails')->name('DeletePaymentDetails');
-
-
-    });
     
 });
 
 
+Route::controller(ServiceProviderController::class)->group(function () {
+    Route::post('BasicInfo', 'BasicInfo')->name('BasicInfo');
+    Route::post('UpdateBasicInfo', 'UpdateBasicInfo')->name('UpdateBasicInfo');
 
+    Route::post('PriceAndPackage', 'PriceAndPackage')->name('PriceAndPackage');
+    Route::post('UpdatePriceAndPackage', 'UpdatePriceAndPackage')->name('UpdatePriceAndPackage');
+
+    Route::post('MediaUpload', 'MediaUpload')->name('MediaUpload');
+    Route::post('UpdateMediaUpload', 'UpdateMediaUpload')->name('UpdateMediaUpload');
+
+    Route::get('Deals', 'Deals')->name('Deals');
+    Route::get('Deal/{id}', 'Deal')->name('Deal');
+    Route::get('DeleteDeal/{id}', 'DeleteDeal')->name('DeleteDeal');
+
+    Route::post('DealPublish', 'DealPublish')->name('DealPublish');
+
+
+    Route::post('MyDetails', 'MyDetails')->name('MyDetails');
+
+    Route::post('UpdatePassword', 'UpdatePassword')->name('UpdatePassword');
+
+    Route::post('BusinessProfile', 'BusinessProfile')->name('BusinessProfile');
+
+
+    Route::post('AddPaymentDetails', 'AddPaymentDetails')->name('AddPaymentDetails');
+    Route::post('UpdatePaymentDetails', 'UpdatePaymentDetails')->name('UpdatePaymentDetails');
+
+    Route::get('DeletePaymentDetails/{id}', 'DeletePaymentDetails')->name('DeletePaymentDetails');
+
+    Route::post('AdditionalPhotos', 'AdditionalPhotos')->name('AdditionalPhotos');
+});
 
 
