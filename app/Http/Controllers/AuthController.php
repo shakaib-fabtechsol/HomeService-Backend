3b509b986c172a10594fa8061c3c121dac00b714
sagar-fabtechsol <sagar.sabir@fabtechsol.com>
2025-02-05 14:37:19 +0500

    added response.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,6 +25,7 @@ class AuthController extends Controller
 
         $user->update(['terms' => $request->terms]);
         return response()->json([
+            'success' => 'User successfully updated!',
             'user' => $user,
         ], 201);
     }
