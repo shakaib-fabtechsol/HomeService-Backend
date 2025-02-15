5adc78746bd98e32fc3b8615016e39f3a1abe021
AliHassanFabtechsol <alihassandaska@fabtechsol.com>
2025-02-15 16:51:11 +0500

    Api for update information on Sales Rap panel

--- app/Http/Controllers/SaleRapController.php
+++ app/Http/Controllers/SaleRapController.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
 class SaleRapController extends Controller
 {
     public function Dashboard(){
@@ -38,5 +40,42 @@ class SaleRapController extends Controller
         }
     }
 
+    public function SalesPersonal(Request $request)
+    {
+        $user = User::find($request->id);
+        if ($user) {
+            $data = $request->all();
+            if ($request->hasFile('personal_image')) {
+                $imagePath = public_path('uploads/' . $user->personal_image);
+                if (!empty($user->personal_image) && file_exists($imagePath)) {
+                    unlink($imagePath);
+                }
+                $photo1 = $request->file('personal_image');
+                $photo_name1 = time() . '-' . $photo1->getClientOriginalName();
+                $photo_destination = public_path('uploads');
+                $photo1->move($photo_destination, $photo_name1);
+                $data['personal_image'] = $photo_name1;
+                $user->update($data);
+            }
+            return response()->json(['message' => 'User Personal details updated successfully', 'user' => $user], 200);
+        } else {
+            return response()->json(['message' => 'No user found'], 200);
+        }
+    }
+
+    public function SalesSecurity(Request $request)
+    {
+        $user = User::find($request->id);
+        if ($user) {
+            if (!Hash::check($request->current_password, $user->password)) {
+                return response()->json(['message' => 'Current password is incorrect'], 200);
+            }
+            $user->password = Hash::make($request->password);
+            $user->save();
+            return response()->json(['message' => 'User Password Updated successfully', 'user' => $user], 200);
+        } else {
+            return response()->json(['message' => 'No user found'], 200);
+        }
+    }
 
 }
